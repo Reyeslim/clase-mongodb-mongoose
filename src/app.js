abb6151f0358a743cf3cf0ed8778fc298e5fbf58
Reyes Limones <reyeslimones@gmail.com>
2026-06-08 15:53:53 +0200

    ready

--- src/app.js
+++ src/app.js
@@ -1,18 +1,9 @@
 import express from "express"
-import reviewRoutes from "./routes/reviewRoutes.js"
-import wishlistRoutes from "./routes/wishlistRoutes.js"
 
 const app = express()
 
 app.use(express.json())
 
-// AÑADIR DESPUÉS DE LAS RUTAS CONTROLLERS ETC
-app.use(reviewRoutes)
-app.use(wishlistRoutes)
-// NOS VAMOS A POSTMAN Y PROBAMOS
-// finnnnn
-
-// QUITAR LUEGO
 app.get("/", (req, res) => {
   res.json({
     ok: true,
@@ -21,5 +12,3 @@ app.get("/", (req, res) => {
 })
 
 export default app
-
-// VEMOS LO QUE HAY PREPARADO Y NOS VAMOS A SERVER.JS
--- src/server.js
+++ src/server.js
@@ -1,32 +1,7 @@
-// src/server.js
-import "dotenv/config" // dependencia para leer variables de entorno
 import app from "./app.js"
-import { dbConnection } from "./db/database.js"
 
 const PORT = process.env.PORT || 3000
 
-// Esto es con lo que empezamos
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`)
-// })
-
-// Esto se añade después.
-
-// Segunda vez en server.js. AÑADIMOS:
-try {
-  await dbConnection()
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor en http://localhost:${PORT}`)
-  })
-} catch (error) {
-  console.error("❌ Error al conectar con MongoDB:", error.message)
-  process.exit(1)
-}
-
-// VEMOS LO QUE HAY PREPARADO,
-// LEVANTAMOS SERVER
-// Y CREAMOS CARPETA DB/DATABASE.JS
-
-// 2 VEZ, NOS VAMOS A MODELOS.
-// En la carpeta models, creamos los schemas que tendrá nuestra base de datos.
-// No es más que la estructura de nuestra colección.
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
